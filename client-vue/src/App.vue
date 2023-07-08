29c710a8d9301c5d8df00e0b8cfc6802498990ad
kkpagaev <kkpagaev@gmail.com>
2023-07-09 01:45:48 +0300

    init vue

---
+++ client-vue/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Vue 3 + Vite + TypeScript + Tailwind CSS</h1>
+    <p>Vue 3 + Vite + TypeScript + Tailwind CSS</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
