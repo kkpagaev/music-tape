7a79c9caa33c8a7121c85631ea685c5e815070b2
kkpagaev <kkpagaev@gmail.com>
2023-07-09 05:32:13 +0300

    feat(server/swagger): initialized swagger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
-import Fastify from "fastify"
+import Fastify, { FastifyInstance } from "fastify"
 import * as dotenv from "dotenv"
 import { PrismaClient } from "@prisma/client"
 import { prisma } from "./prisma"
 import formBody from "@fastify/formbody"
+import swagger from "@fastify/swagger"
+import swaggerUi from "@fastify/swagger-ui"
+import fp from "fastify-plugin"
 
 dotenv.config()
 
@@ -12,17 +15,52 @@ declare module "fastify" {
   }
 }
 
+const swaggerPlugin = fp(async (fastify: FastifyInstance) => {
+  await fastify.register(swagger, {
+    swagger: {
+      info: {
+        title: "Test swagger",
+        description: "Testing the Fastify swagger API",
+        version: "0.1.0",
+      },
+      host: "localhost",
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      securityDefinitions: {
+        apiKey: {
+          type: "apiKey",
+          name: "apiKey",
+          in: "header",
+        },
+      },
+    },
+  })
+
+  await fastify.register(swaggerUi, {
+    routePrefix: "/swagger",
+  })
+})
+
 async function main() {
   const fastify = Fastify({
     logger: true,
   })
-  const { plugin, prefix } = await import("./http")
 
   await fastify.register(formBody)
 
+  await fastify.register(swaggerPlugin)
+
+  fastify.get("/hello", async () => {
+    return { hello: "world" }
+  })
+
+  const { plugin, prefix } = await import("./http")
   await fastify.register(plugin, {
     prefix,
   })
+
+  await fastify.ready()
+  fastify.swagger()
 
   // Run the server!
   const start = async () => {
