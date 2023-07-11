464363feaa92fa9ed1e1cb5201359ef3251d106b
kkpagaev <kkpagaev@gmail.com>
2023-07-11 08:23:06 +0300

    feat(cliend): added cool packages

---
+++ client-vue/src/App2.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const foo = "test";
+const bar = "test2";
+const baz = "test3";
+
+console.log(foo);
+console.log(baz);
+console.log(bar);
+</script>
+
+<template>
+  <div>
+    {{ foo }}
+    {{ bar }}
+  </div>
+</template>
